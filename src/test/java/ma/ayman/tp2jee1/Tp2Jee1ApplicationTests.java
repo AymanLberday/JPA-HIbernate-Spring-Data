8dcf23e43dd722e6ac675e18742b076a270a80ca
Ayman <ayman@gmail.com>
2024-04-01 16:56:16 +0200

    First Commit

---
+++ src/test/java/ma/ayman/tp2jee1/Tp2Jee1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ayman.tp2jee1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2Jee1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
